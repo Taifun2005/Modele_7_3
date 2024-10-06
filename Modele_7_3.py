6975b2b8448c668c78bd48800a89544e1f12e2d8
Taifun2005 <lider2005@yandex.ru>
2024-10-06 20:01:44 +0700

    Add files via upload

--- Modele_7_3.py
+++ Modele_7_3.py
@@ -28,11 +28,29 @@ class WordsFinder:
             all_words[self.name] = line.split()
         return all_words
 
-    def find(self, word):
-        self.all_words
+    def find(self, word):                   # Методы find(),       print(s.find('foo'))
+        slovar = self.get_all_words().values()
+        stroka = str(*slovar)
+        coint = 0
+        print(slovar)
+        print(type(slovar))
+        for i range(len(stroka)):
+            if stroka != word:
+                coint += 1
 
-    def count(self, word):
+        # slovar = *slovar
+        # count = stroka.find(word.lower())
+        # print(slovar)
+        # print(type(slovar))
+        # print(word.lower())
+        return count
 
+
+    def count(self, word):            # Метод count()  print(s.count('oo'))
+        slovar = self.get_all_words().values()
+        stroka = str(slovar)
+        count = stroka.count(word.lower())
+        return count
 
 
 finder2 = WordsFinder('test_file.txt')
@@ -40,4 +58,4 @@ finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
 
 print(finder2.find('TEXT')) # 3 слово по счёту
-# print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
